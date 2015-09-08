f41a44888f3784f821f7c6619f9227b960f0af7f
dick-ey <dick_ey@rgnt2-120-160-dhcp.int.colorado.edu>
2015-09-08 09:07:09 -0600

    app state changes
    nothing major

---
+++ Examples and Tests/Intro to Swift.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let fName = "Matt"
+let lName = "Dickey"
+var age = 24
+
+println(fName + " " + lName)
+
+
+
+println("My Name is \(fName)  \(lName). ")
+
+
+if age < 21{
+    println("No beer for you, little man!")
+} else {
+    println("Have a drink.")
+}
+
+
+for count in 0...age{
+    println(count)
+}
+
