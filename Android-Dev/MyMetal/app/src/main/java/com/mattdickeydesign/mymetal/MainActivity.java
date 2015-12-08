37366a69a1a9e19c05e8c233fec827c543f68dd9
dick-ey <dick.ey@icloud.com>
2015-12-08 12:45:35 -0700

    Added Working Project 3

--- Android-Dev/MyMetal/app/src/main/java/com/mattdickeydesign/mymetal/MainActivity.java
+++ Android-Dev/MyMetal/app/src/main/java/com/mattdickeydesign/mymetal/MainActivity.java
@@ -1,61 +1,83 @@
 package com.mattdickeydesign.mymetal;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.AdapterView;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.view.View;
 import android.widget.ToggleButton;
 
 
-
-
 public class MainActivity extends AppCompatActivity {
 
-
-    public class SpinnerActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
-
-        @Override
-        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-            // code here
-
-            String test =
-        }
-
-        @Override
-        public void onNothingSelected(AdapterView<?> parent) {
-            //code here for nothing selected
-        }
-    }
-
-    Spinner genreSpinner = (Spinner) findViewById(R.id.genreSpinner);
-    String chosenGenre = String.valueOf(genreSpinner.getSelectedItem());
+    public void updateChecks(View view) {
 
 
-
-    public void getMetal(View view) {
-
-// GLOBAL VARIABLES
-        EditText textField = (EditText) findViewById(R.id.nameTextField);
-        String userName = textField.getText().toString();
-
-        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
-        boolean preference = toggleButton.isChecked();
-        String prefText;
-
-//        Spinner genreSpinner = (Spinner) findViewById(R.id.genreSpinner);
-//        String chosenGenre = String.valueOf(genreSpinner.getSelectedItem());
+        Spinner genreSpinner = (Spinner) findViewById(R.id.genreSpinner);
+        String chosenGenre = String.valueOf(genreSpinner.getSelectedItem());
 
         CheckBox check0 = (CheckBox) findViewById(R.id.checkBox0);
         CheckBox check1 = (CheckBox) findViewById(R.id.checkBox1);
         CheckBox check2 = (CheckBox) findViewById(R.id.checkBox2);
         CheckBox check3 = (CheckBox) findViewById(R.id.checkBox3);
 
+////CHECKBOX LOGIC
+        String[] hardcoreBands = getResources().getStringArray(R.array.hardcoreBands);
+        String[] metalcoreBands = getResources().getStringArray(R.array.metalcoreBands);
+        String[] deathcoreBands = getResources().getStringArray(R.array.deathcoreBands);
+        String[] classicBands = getResources().getStringArray(R.array.classicBands);
+
+
+        if (chosenGenre.contentEquals("Hardcore")) {
+            check0.setText(hardcoreBands[0]);
+            check1.setText(hardcoreBands[1]);
+            check2.setText(hardcoreBands[2]);
+            check3.setText(hardcoreBands[3]);
+        } else if (chosenGenre.contentEquals("Metalcore")) {
+            check0.setText(metalcoreBands[0]);
+            check1.setText(metalcoreBands[1]);
+            check2.setText(metalcoreBands[2]);
+            check3.setText(metalcoreBands[3]);
+        } else if (chosenGenre.contentEquals("Deathcore")) {
+            check0.setText(deathcoreBands[0]);
+            check1.setText(deathcoreBands[1]);
+            check2.setText(deathcoreBands[2]);
+            check3.setText(deathcoreBands[3]);
+        } else if (chosenGenre.contentEquals("Classic")) {
+            check0.setText(classicBands[0]);
+            check1.setText(classicBands[1]);
+            check2.setText(classicBands[2]);
+            check3.setText(classicBands[3]);
+        } else {
+            System.out.println(" java ln 60 : Error occurred updating checkboxes");
+        }
+    }
+
+
+    public void getMetal(View view) {
+
+// GLOBAL VARIABLES
+        EditText textField = (EditText) findViewById(R.id.nameTextField);
+        String userName;
+        userName = textField.getText().toString();
+
+        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+        boolean preference = toggleButton.isChecked();
+        String prefText;
+
         TextView sentence = (TextView) findViewById(R.id.textView);
+
+        Spinner updatedSpinnerGenre = (Spinner) findViewById(R.id.genreSpinner);
+        String spinnerString = updatedSpinnerGenre.getSelectedItem().toString();
+
+        CheckBox check0 = (CheckBox) findViewById(R.id.checkBox0);
+        CheckBox check1 = (CheckBox) findViewById(R.id.checkBox1);
+        CheckBox check2 = (CheckBox) findViewById(R.id.checkBox2);
+        CheckBox check3 = (CheckBox) findViewById(R.id.checkBox3);
 
 
 // TOGGLE LOGIC
@@ -64,24 +86,41 @@ public class MainActivity extends AppCompatActivity {
         } else {
             prefText = " doesn't like";
         }
-//CHECKBOX LOGIC
-        String[] metalGenres = getResources().getStringArray(R.array.genres);
-        String[] hardcoreBands = getResources().getStringArray(R.array.hardcoreBands);
-        String[] metalcoreBands = getResources().getStringArray(R.array.metalcoreBands);
-        String[] deathcoreBands = getResources().getStringArray(R.array.deathcoreBands);
-        String[] classicBands = getResources().getStringArray(R.array.classicBands);
 
-        if (chosenGenre.contentEquals("Hardcore")) {
-            check0.setText(hardcoreBands[0]);
-        }else{
-            System.out.println("you done fucked up, Dick");
+// GET CHECKED CHECKBOX VALUES
+
+        String bandExamples = "";
+        boolean checked0 = check0.isChecked();
+        boolean checked1 = check1.isChecked();
+        boolean checked2 = check2.isChecked();
+        boolean checked3 = check3.isChecked();
+
+        if (checked0) {
+            bandExamples += check0.getText().toString() + ", ";
+        }
+        if (checked1) {
+            bandExamples += check1.getText().toString() + ", ";
+        }
+        if (checked2) {
+            bandExamples += check2.getText().toString() + ", ";
+        }
+        if (checked3) {
+            bandExamples += check3.getText().toString() + ".";
         }
 
+
 //SET TEXT FOR SENTENCE
-        sentence.setText("");
+        sentence.setText(userName + " " + prefText + " " + spinnerString + " metal. This includes bands like: " + bandExamples);
+
+//CHANGE THE IMAGE VIEW
+        ImageView metalHandView = (ImageView) findViewById(R.id.image);
+        metalHandView.setImageResource(R.drawable.hand);
 
 
     }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ Projects/Project 3/MultiIntent/app/src/main/java/com/mattdickeydesign/multiintent/MainActivity.java
@@ -0,0 +1,77 @@
+package com.mattdickeydesign.multiintent;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner genreSpinner = (Spinner) findViewById(R.id.spinner);
+    String chosenGenre = String.valueOf(genreSpinner.getSelectedItem());
+    Button getlinks = (Button) findViewById(R.id.button);
+    String buttonText = String.valueOf(getlinks.getText());
+
+
+
+    public int randomize(){
+        // Make a random number between 1 and whatever. return the number
+    }
+
+
+    public void getLinks(int, View view){
+
+    }
+
+
+
+    public static
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
